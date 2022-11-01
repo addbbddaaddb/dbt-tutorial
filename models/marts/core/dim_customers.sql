0f91ebd9dd132d6f90bd57952592aff2c76d2b14
Arnault Droz-dit-Busset <arnault.droz@gmail.com>
2022-11-01 03:27:52 +0000

    14

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,7 +6,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders')}}
 
 ),
 
@@ -17,53 +17,25 @@ payments as (
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
-    group by customer_id
-
+    group by 1
 ),
-
-customer_payments as (
-
-    select
-        orders.customer_id,
-        sum(amount) as total_amount
-
-    from payments
-
-    left join orders on
-         payments.order_id = orders.order_id
-
-    group by orders.customer_id
-
-),
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
-        customer_orders.first_order,
-        customer_orders.most_recent_order,
-        customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
-
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
-
-    left join customer_orders
-        on customers.customer_id = customer_orders.customer_id
-
-    left join customer_payments
-        on  customers.customer_id = customer_payments.customer_id
-
+    left join customer_orders using (customer_id)
 )
-
 select * from final
