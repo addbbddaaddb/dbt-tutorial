c06a484d6febd2d9a548838d5c94fe3761e4a074
Arnault Droz-dit-Busset <arnault.droz@gmail.com>
2022-11-16 14:19:13 +0000

    s

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -10,9 +10,8 @@ with
             order_id,
             sum(case when status = 'success' then amount end) as amount,
             {%- for payment_method in payment_methods %}
-            sum(
-                case when payment_method = '{{payment_method}}' then amount end
-            ) as {{ payment_method }}_amount
+            sum(case when payment_method = '{{payment_method}}' then amount end) as {{ payment_method }}_amount
+           
             {%- if not loop.last %},{% endif -%}
             {% endfor %}
         from payments
@@ -27,8 +26,8 @@ with
             orders.order_date,
             coalesce(order_payments.amount, 0) as amount,
             {% for payment_method in payment_methods -%}
-            order_payments.{{ payment_method }}_amount,
-            {% endfor -%}
+            order_payments.{{ payment_method }}_amount
+            {%- for not loop.last %},{% endfor -%}
 
         from orders
         left join order_payments using (order_id)
